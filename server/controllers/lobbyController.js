5e63c84c83ffe9ec590ad8024922feaf879ec658
Popov-Dmitry <popov.dmitry.dev@gmail.com>
2021-12-26 01:19:14 +0700

    Added lobbyController

---
+++ server/controllers/lobbyController.js
@@ -0,0 +1,36 @@
+const lobbyService = require("../services/lobbyService");
+
+class LobbyController {
+    async create(req, res) {
+        try {
+            let {title, description, isPrivate, ownerId, giftPrice, currency} = req.body;
+            let lobby = await lobbyService.create(title, description, isPrivate, ownerId, giftPrice, currency);
+            return res.status(201).json(lobby);
+        }
+        catch (e) {
+            return res.status(400).json(e);
+        }
+    }
+
+    async getAllPublic(req, res) {
+        try {
+            let lobbies = await lobbyService.findAllPublic();
+            return res.status(200).json(lobbies);
+        }
+        catch (e) {
+            return res.status(500).json(e);
+        }
+    }
+
+    async getById(req, res) {
+        try {
+            let lobby = await lobbyService.findById(req.params.id);
+            return res.status(200).json(lobby);
+        }
+        catch (e) {
+            return res.status(404).json(e);
+        }
+    }
+}
+
+module.exports = new LobbyController();
