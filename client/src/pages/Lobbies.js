e1a141736fe064d11bb06847f4117aa7d7d15239
Popov-Dmitry <popov.dmitry.dev@gmail.com>
2021-12-31 19:31:45 +0700

    Added Lobbies page

---
+++ client/src/pages/Lobbies.js
@@ -0,0 +1,29 @@
+import React, {useContext, useEffect} from 'react';
+import {fetchAllPublic} from "../http/lobbyApi";
+import {Context} from "../index";
+import {observer} from "mobx-react-lite";
+import {Container} from "react-bootstrap";
+import LobbiesList from "../components/LobbiesList";
+
+const Lobbies = observer(() => {
+    const {lobbies} = useContext(Context);
+
+    useEffect(() => {
+        fetchAllPublic().then(({data, status, statusText}) => {
+            if (status === 200) {
+                lobbies.setLobbies(data);
+            }
+            else {
+                alert(statusText + "\n" + data);
+            }
+        });
+    }, []);
+
+    return (
+        <Container>
+            <LobbiesList/>
+        </Container>
+    );
+});
+
+export default Lobbies;
