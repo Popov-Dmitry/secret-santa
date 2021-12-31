9157bfe06af3145ab87fa3c3782472d9af0dfbf5
Popov-Dmitry <popov.dmitry.dev@gmail.com>
2021-12-31 15:45:05 +0700

    Added pairApi

--- client/src/http/participantApi.js
+++ client/src/http/participantApi.js
@@ -1,4 +1,4 @@
-import {$authHost} from "./index";
+import {$authHost, $host} from "./index";
 import {ADD_PARTICIPANT, FETCH_PARTICIPANTS, FETCH_PARTICIPANTS_BY_LOBBY_ID} from "../utils/endpoints";
 
 export const add = async (wishes, address, userId, lobbyId) => {
@@ -12,6 +12,6 @@ export const fetchById = async (id) => {
 }
 
 export const fetchByLobbyId = async (lobbyId) => {
-    const {data, status, statusText} = await $authHost.get(FETCH_PARTICIPANTS_BY_LOBBY_ID + "/" + lobbyId);
+    const {data, status, statusText} = await $host.get(FETCH_PARTICIPANTS_BY_LOBBY_ID + "/" + lobbyId);
     return {data, status, statusText};
 }
