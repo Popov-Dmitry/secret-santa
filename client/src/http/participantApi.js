5ef86e79e330c9d77279b8b6c035bb9306183a78
Popov-Dmitry <popov.dmitry.dev@gmail.com>
2021-12-31 15:40:40 +0700

    Added participantApi

---
+++ client/src/http/participantApi.js
@@ -0,0 +1,17 @@
+import {$authHost} from "./index";
+import {ADD_PARTICIPANT, FETCH_PARTICIPANTS, FETCH_PARTICIPANTS_BY_LOBBY_ID} from "../utils/endpoints";
+
+export const add = async (wishes, address, userId, lobbyId) => {
+    const {data, status, statusText} = await $authHost.post(ADD_PARTICIPANT, {wishes, address, userId, lobbyId});
+    return {data, status, statusText};
+}
+
+export const fetchById = async (id) => {
+    const {data, status, statusText} = await $authHost.get(FETCH_PARTICIPANTS + "/" + id);
+    return {data, status, statusText};
+}
+
+export const fetchByLobbyId = async (lobbyId) => {
+    const {data, status, statusText} = await $authHost.get(FETCH_PARTICIPANTS_BY_LOBBY_ID + "/" + lobbyId);
+    return {data, status, statusText};
+}
--- client/src/http/userApi.js
+++ client/src/http/userApi.js
@@ -1,14 +1,24 @@
 import {$host} from "./index";
 import jwtDecode from "jwt-decode";
+import {ADD_USER, FETCH_USERS, LOGIN_USER} from "../utils/endpoints";
 
 export const registration = async (email, password, fullName) => {
-    const {data} = await $host.post("/api/users/", {email, password, fullName});
-    localStorage.setItem("token", data.token);
-    return jwtDecode(data.token);
+    const {data, status, statusText} = await $host.post(ADD_USER, {email, password, fullName});
+    if (status === 200) {
+        localStorage.setItem("token", data.token);
+    }
+    return {data, status, statusText};
 }
 
 export const login = async (email, password) => {
-    const {data} = await $host.post("/api/users/login", {email, password});
-    localStorage.setItem("token", data.token);
-    return jwtDecode(data.token);
+    const {data, status, statusText} = await $host.post(LOGIN_USER, {email, password});
+    if (status === 200) {
+        localStorage.setItem("token", data.token);
+    }
+    return {data, status, statusText};
+}
+
+export const fetchById = async (id) => {
+    const {data, status, statusText} = await $host.get(FETCH_USERS + "/" + id);
+    return {data, status, statusText};
 }
