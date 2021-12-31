781206333552adbc8a734c7cc3fef9205defd3b0
Popov-Dmitry <popov.dmitry.dev@gmail.com>
2021-12-31 17:11:40 +0700

    Added LobbyStore

--- client/src/index.js
+++ client/src/index.js
@@ -2,11 +2,15 @@ import React, {createContext} from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import UserStore from "./store/UserStore";
+import LobbyStore from "./store/LobbyStore";
 
 export const Context = createContext(null);
 
 ReactDOM.render(
-    <Context.Provider value={{user: new UserStore()}}>
+    <Context.Provider value={{
+        user: new UserStore(),
+        lobbies: new LobbyStore()
+    }}>
         <App />
     </Context.Provider>,
   document.getElementById('root')
