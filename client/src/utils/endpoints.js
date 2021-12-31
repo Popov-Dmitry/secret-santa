cb2e57f5e45213254721a7261c54c27750080e7f
Popov-Dmitry <popov.dmitry.dev@gmail.com>
2021-12-31 14:14:53 +0700

    Added endpoints

---
+++ client/src/utils/endpoints.js
@@ -0,0 +1,11 @@
+export const ADD_USER = "/api/users";
+export const FETCH_USERS = "/api/users";
+export const ADD_LOBBY = "/api/lobbies";
+export const FETCH_LOBBIES = "/api/lobbies";
+export const FETCH_LOBBIES_BY_INVITE_CODE = "/api/lobbies/invite";
+export const ADD_PARTICIPANT = "/api/participants";
+export const FETCH_PARTICIPANTS = "/api/participants";
+export const FETCH_PARTICIPANTS_BY_LOBBY_ID = "/api/participants/lobby";
+export const PAIR_UP = "/api/pairs";
+export const FETCH_PAIR_BY_FROM_ID = "/api/pairs";
+export const FETCH_PAIRS_BY_LOBBY_ID = "/api/pairs/lobbies";
